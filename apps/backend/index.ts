ee8052525ec6476faea516c9f9cd21a5e60f2ded
Alexandre Bacha <alexandre.bacha@epitech.eu>
2025-11-29 19:28:04 +0100

    fix : index with conflicts

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,12 +1,11 @@
-import { Elysia } from "elysia";
-import { cors } from "@elysiajs/cors";
-import openapi from "@elysiajs/openapi";
-import { PrismaClient } from "@area/shared";
+import { Elysia } from "elysia"
+import { cors } from "@elysiajs/cors"
+import openapi from "@elysiajs/openapi"
 
 const app = new Elysia()
   .use(cors())
   .use(openapi())
-  .listen(8080);
+  .listen(8080)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
 
