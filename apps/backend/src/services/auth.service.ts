90ea07ffcb237e42d4d27bbb86b4a0e81b10fa0d
Aurélien <aurelien.lamberger@epitech.eu>
2025-12-02 17:23:20 +0100

    feat(auth service) getUserById

--- apps/backend/src/services/auth.service.ts
+++ apps/backend/src/services/auth.service.ts
@@ -4,6 +4,27 @@ import { hashPassword } from "../middlewares/password"
 
 export class AuthService {
 
+    // signIn(email, password):
+    // register(username, email, password)
+    // isPasswordAndEmailCorrect()
+    // créer un token pour verifier l'email de l'utilisateur (verfiier si il a reçu un mail)
+    // updateExpiredToken(refreshToken)
+    // getAccessToken(refreshToken)
+
+    //  savegarder un refresh token
+    //  verifier si un refresh token est valid
+
+    async getUserById(data: {
+        id: string
+    }) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: data.id
+            },
+        })
+        return user
+    }
+
     async emailExist(data: {
         email: string
     }) {
@@ -30,6 +51,7 @@ export class AuthService {
         })
         return user
     }
+    //async emailTokenVerification()
 }
 
 export const authService = new AuthService()
