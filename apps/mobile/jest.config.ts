934c4d0f601a40c8dd2a8e4f4c688f8ea2fe007c
Mateo Cabrera <mateo.cabrera@epitech.eu>
2026-01-03 01:46:38 +0100

    chore(mobile): migrate config files to TypeScript

--- apps/mobile/jest.config.ts
+++ apps/mobile/jest.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
   preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!.*((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|nativewind|react-native-reanimated))"
@@ -8,3 +10,5 @@ module.exports = {
     "^@/(.*)$": "<rootDir>/$1"
   }
 };
+
+export default config;
